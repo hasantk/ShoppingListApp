c4c331a57eec61b749c6ad0d0c7d7bd779b3044a
Hasan Atık <hasan.atik16860@gmail.com>
2022-02-08 02:36:03 -0800

    Initial Commit

---
+++ ShoppingList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShoppingList
+//
+//  Created by Hasan Atık on 8.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
